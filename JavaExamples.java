44f4a70b32fdb4dd1717a4f535ff6933c9d5352a
KishaKandha <kishanik1118@gmail.com>
2020-09-19 17:14:34 +0530

    This is done by master

---
+++ JavaExamples.java
@@ -0,0 +1,36 @@
+public class JavaExamples
+{
+	int questionNo;
+	string category;
+	String text;
+	
+	
+	public void setQuestionNo(int Qno)
+	{
+	
+		
+	}
+	public void setCategory(String category)
+	{
+		
+		
+	}
+	public void setText(String text)
+	{
+		
+		
+	}
+	
+	public int getQuestionNo()
+	{
+	      return 0;
+	} 
+	public String getCategory()
+	{
+		return ;
+	}
+	public String getText()
+	{   
+	     return  ;   
+	}
+}	
